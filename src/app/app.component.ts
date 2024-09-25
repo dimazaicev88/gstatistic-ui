e0bdb16b112a2fdb0301836558b959faf2bf644e
dimaz <dimazaicev88@gmail.com>
2024-09-25 18:10:22 +0300

    fix changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LeftMenuComponent} from "./components/left-menu/left-menu.component";
 import {StatisticPageComponent} from "./statistic/pages/statistic/statistic-page.component";
+import {HitsPageComponent} from "./visitors/pages/hits/hits-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LeftMenuComponent, StatisticPageComponent],
+  imports: [RouterOutlet, LeftMenuComponent, StatisticPageComponent, HitsPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/visitors/pages/hits/hits-page.component.ts
+++ src/app/visitors/pages/hits/hits-page.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {InputTextModule} from "primeng/inputtext";
+import {DropdownModule} from "primeng/dropdown";
+import {RadioButtonModule} from "primeng/radiobutton";
+import {ButtonDirective} from "primeng/button";
 
 @Component({
   selector: 'app-hits-page',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    InputTextModule,
+    DropdownModule,
+    RadioButtonModule,
+    ButtonDirective
+  ],
   templateUrl: './hits-page.component.html',
   styleUrl: './hits-page.component.css'
 })
 export class HitsPageComponent {
+  filterData = {
+    userId: '',
+    hitId: '',
+    sessionId: '',
+    date: null,
+    visitorId: '',
+    registered: null,
+    blacklist: null,
+    logic: 'and'
+  };
 
+  dateOptions = [
+    {label: 'Месяц', value: 'month'},
+    {label: 'Неделя', value: 'week'},
+    {label: 'День', value: 'day'}
+  ];
+
+  registrationOptions = [
+    {label: '(все)', value: null},
+    {label: 'Да', value: true},
+    {label: 'Нет', value: false}
+  ];
+
+  blacklistOptions = [
+    {label: '(все)', value: null},
+    {label: 'Да', value: true},
+    {label: 'Нет', value: false}
+  ];
 }
